01884cff97708a079849bbb174f7cb69ea5c3e0f
Ruben Laube-Pohto <ruben.laubepohto@gmail.com>
2017-08-08 10:33:51 +0300

    Add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  exports: [TestComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class GhModule { }

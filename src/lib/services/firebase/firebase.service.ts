137856b13a8cc9413dcb857ae3c9168721c26fed
Ruben Laube-Pohto <ruben.laubepohto@gmail.com>
2017-08-09 15:26:22 +0300

    Leaner import

--- src/lib/services/firebase/firebase.service.ts
+++ src/lib/services/firebase/firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of'
 
 import { Provider, User } from '../../models';
 

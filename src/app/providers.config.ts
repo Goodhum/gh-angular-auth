dd7629f9312ba631fdccdf81f8d0af6634fcaf62
Ruben Laube-Pohto <ruben.laubepohto@gmail.com>
2017-08-10 19:26:33 +0300

    Add config file

---
+++ src/app/providers.config.ts
@@ -0,0 +1,14 @@
+export const config = {
+    firebase: {
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: ''
+    },
+    auth0: {
+        domain: '',
+        client_id: ''
+    }
+};
